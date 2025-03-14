1a509bc7961c10f32e91bc9440c149cf6a48b9ae
Joe Ulyatt <joewhocodes@gmail.com>
2025-03-14 11:00:10 +0100

    feat: :sparkles: Update UI of Chat and update gitignore

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,19 @@
 import axios from 'axios'
 import { useState } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { FlatList, Text, TouchableOpacity } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { GeminiResponse } from '../../types/gemini'
 
 export default function HomeScreen() {
-	const [message, setMessage] = useState('')
-
+	const [messages, setMessages] = useState<string[]>([])
 	const getChat = () => {
 		const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.EXPO_PUBLIC_GEMINI_API_KEY}`
 
 		const requestBody = {
 			contents: [
 				{
-					parts: [{ text: 'Write a story about a magic backpack.' }],
+					// parts: [{ text: 'Write a story about a magic backpack.' }],
+					parts: [{ text: 'Hey Gemini' }],
 				},
 			],
 		}
@@ -26,7 +27,7 @@ export default function HomeScreen() {
 			.then(response => {
 				const generatedText =
 					response.data.candidates[0]?.content.parts[0]?.text || ''
-				setMessage(generatedText)
+				setMessages(prevMessages => [...prevMessages, generatedText])
 				console.log('Generated text:', generatedText)
 			})
 			.catch((error: { response: { data: any }; message: any }) => {
@@ -38,14 +39,19 @@ export default function HomeScreen() {
 	}
 
 	return (
-		<View className='flex-1 justify-center items-center'>
+		<SafeAreaView className='flex-1 justify-center items-center p-4 gap-y-4'>
+			<Text className='text-2xl font-bold'>Chat with AI</Text>
+			<FlatList
+				className='flex-1 w-full border-2 border-red-500 flex-row'
+				data={messages}
+				renderItem={({ item }) => <Text>{item}</Text>}
+			/>
 			<TouchableOpacity
 				className='bg-red-500 p-4 rounded-lg'
 				onPress={getChat}
 			>
 				<Text className='text-white'>Generate AI Story</Text>
 			</TouchableOpacity>
-			{message && <Text className='p-4 mt-4'>{message}</Text>}
-		</View>
+		</SafeAreaView>
 	)
 }
